8f2231c3eb447c30cf9cef479629586272788f86
Sachintha-Lakruwan <sachi.new2002@gmail.com>
2023-12-10 20:30:43 +0530

    set the title

--- pages/index.js
+++ pages/index.js
@@ -5,23 +5,29 @@ import TrendingHeading from "@/components/home/TrendingHeading";
 import ItemGrid from "@/components/product-cards/ItemGrid";
 import { cupCakeData } from "@/public/cup-cake-data";
 import { cakeData } from "@/public/cake-data";
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div>
-      <Hero />
-      <div className="w-full flex pb-[10vh] flex-col items-center">
-        <div className=" flex flex-col items-center  w-5/6 sm:w-3/5 lg:w-3/4">
-          <Feaures />
-          <TrendingHeading category={"Cup-Cakes"} link={"cup-cakes"} />
-          <ItemGrid products={cupCakeData} />
-        </div>
-        <Poster />
-        <div className=" flex flex-col items-center  w-5/6 sm:w-3/5 lg:w-3/4">
-          <TrendingHeading category={"Cakes"} link={"cakes"} />
-          <ItemGrid products={cakeData} />
+    <>
+      <Head>
+        <title>Cake Craft</title>
+      </Head>
+      <div>
+        <Hero />
+        <div className="w-full flex pb-[10vh] flex-col items-center">
+          <div className=" flex flex-col items-center  w-5/6 sm:w-3/5 lg:w-3/4">
+            <Feaures />
+            <TrendingHeading category={"Cup-Cakes"} link={"cup-cakes"} />
+            <ItemGrid products={cupCakeData} />
+          </div>
+          <Poster />
+          <div className=" flex flex-col items-center  w-5/6 sm:w-3/5 lg:w-3/4">
+            <TrendingHeading category={"Cakes"} link={"cakes"} />
+            <ItemGrid products={cakeData} />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
