e882d03eb8e478316078806bc0b9c14085781552
Sachintha-Lakruwan <sachi.new2002@gmail.com>
2023-12-10 15:54:34 +0530

    snow efect

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Footer from "../footer/Footer";
 import MainNavigation from "../navbar/MainNavigation";
+import Snowfall from "react-snowfall";
 import { Lato } from "next/font/google";
 const lato = Lato({
   subsets: ["latin"],
@@ -11,8 +12,9 @@ function Layout({ children }) {
   const [navBar, setNavBar] = useState(false);
   return (
     <div
-      className={`w-full min-h-screen grid grid-rows-[auto_1fr_auto] ${lato.className}`}
+      className={`w-full min-h-screen grid grid-rows-[auto_1fr_auto] ${lato.className} relative`}
     >
+      <Snowfall style={{ zIndex: 55 }} snowflakeCount={300} />
       <MainNavigation navBar={navBar} setNavBar={setNavBar} />
       <main className="bg-gray-200" onClick={() => setNavBar(false)}>
         {children}
