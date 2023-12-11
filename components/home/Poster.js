6a2e7f114d6ee6db608f97a235cef87c2ddcb853
Sachintha-Lakruwan <sachi.new2002@gmail.com>
2023-12-11 22:46:42 +0530

    poster padding adjusted

--- components/home/Poster.js
+++ components/home/Poster.js
@@ -9,7 +9,7 @@ const poppins = Poppins({
 
 function Poster() {
   return (
-    <div className=" my-20 bg-white w-full py-10 flex justify-center">
+    <div className=" my-20 bg-white w-full py-20 lg:py-10 flex justify-center">
       <div className=" flex justify-center w-5/6 sm:w-3/5 lg:w-3/4 md:gap-[2%]">
         <div className="hidden w-[49%] aspect-[4/5] gradient lg:inline-block">
           <Text />
